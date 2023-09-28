7aaba95a9fb66fa40da4670f2d5cb5c8991d5a94
Juan Santos <107580945+JuanSantos64@users.noreply.github.com>
2023-09-28 15:51:14 -0300

    RickandMortyAPIFinished

---
+++ Rick and Morty API/Rick and Morty API/Rick_and_Morty_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_Morty_APIApp.swift
+//  Rick and Morty API
+//
+//  Created by Student11 on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Rick_and_Morty_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
